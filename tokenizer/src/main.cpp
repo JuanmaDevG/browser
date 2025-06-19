6a65739df931837c8fd271e808b0b71bf00f0362
JuanmaDevG <juanmagonzalezmartinez@protonmail.com>
2025-06-19 20:01:43 +0200

    Minal changes

--- tokenizer/src/main.cpp
+++ tokenizer/src/main.cpp
@@ -5,10 +5,36 @@
 
 using namespace std;
 
+///////// Comprobación de que vacíe la lista resultado
+
+void imprimirListaSTL(const list<string>& cadena)
+{
+        list<string>::const_iterator itCadena;
+        for(itCadena=cadena.begin();itCadena!=cadena.end();itCadena++)
+        {
+                cout << (*itCadena) << ", ";
+        }
+        cout << endl;
+}
+
 int
 main(void)
 {
-  Tokenizador tk("$%&/()=._-@", true, true);
-  if(!tk.TokenizarDirectorio("corpus"))
-    cout << "ALGO VA MAL" << endl;
+	bool kCasosEspeciales = true, kpasarAminusculas = false;
+
+	list<string> lt1, lt2;
+
+Tokenizador a("", true, false); 
+list<string> tokens; 
+
+a.Tokenizar("http:", tokens);
+	imprimirListaSTL(tokens);
+
+a.Tokenizar("http:////ab/", tokens);
+	imprimirListaSTL(tokens);
+
+a.Tokenizar("http:////ab.", tokens);
+	imprimirListaSTL(tokens);
+
+
 }
